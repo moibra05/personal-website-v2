783dc5b11ab524b8e4eb97cbb5bcb339b5f32401
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-02-27 13:36:13 -0500

    chore: project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        ptsans: ["var(--font-pt-sans)", "sans-serif"],
+      }
     },
   },
   plugins: [],
