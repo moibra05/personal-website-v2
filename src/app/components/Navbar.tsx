daf386b196015be66dfdc45c7e97c4c9ee2d0b82
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-02-28 23:37:35 -0500

    fix: fixed navbar transitions not being smooth

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -55,6 +55,8 @@ export default function Navbar() {
             <Link
               key={link}
               to={link.toLowerCase()}
+              smooth={true}
+              duration={400}
               onClick={() => setIsMenuOpen(false)}
               className="text-2xl py-2 overflow-hidden"
             >
@@ -81,13 +83,15 @@ export default function Navbar() {
         <div key={link} className="group relative">
           <Link
             key={link}
+            smooth={true}
+            duration={400}
             to={link.toLowerCase()}
-            className="py-2 uppercase font-bold"
+            className="py-2 uppercase font-bold group-hover:text-secondary transition-colors duration-200"
           >
             {link}
           </Link>
           {/* Animated Underline */}
-          <div className="h-[2px] w-full bg-white1 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></div>
+          <div className="h-[2px] w-full bg-secondary scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></div>
         </div>
       ))}
     </div>
