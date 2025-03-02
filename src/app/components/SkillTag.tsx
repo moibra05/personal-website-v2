914131add53cce2c025438cf3866c5c29551d13c
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-02 12:19:52 -0500

    feat: added icons to skill tags

--- src/app/components/SkillTag.tsx
+++ src/app/components/SkillTag.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+
 type Prop = {
   skill: string;
 };
 
 export default function SkillTag({ skill }: Prop) {
   return (
-    <span className="px-3 py-1 bg-secondary font-bold text-white rounded-xl text-base md:text-lg hover:cursor-default hover:scale-105 transition-transform duration-300">
+    <span className="flex items-center px-3 py-1 bg-secondary font-bold text-white rounded-xl text-base md:text-lg hover:cursor-default hover:scale-105 transition-transform duration-300">
+      <Image
+        src={`/tools-icons/${skill
+          .toLowerCase()
+          .replace(/[^a-z0-9]/g, "-")}.png`}
+        alt={skill}
+        width={24}
+        height={24}
+        className="inline-block mr-2 hue-rotate-180 filter invert brightness-200"
+      />
+
       {skill}
     </span>
   );
