17f36afd9a52692c09cb07bc8648dfb0fc1ce7a5
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-02 23:28:57 -0500

    feat: added nodemailer package

--- src/app/components/page_sections/ContactSection.tsx
+++ src/app/components/page_sections/ContactSection.tsx
@@ -5,7 +5,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 export default function ContactSection() {
-  const onSubmit = (value) => {
+  const onSubmit = () => {
     console.log(values);
   };
 
