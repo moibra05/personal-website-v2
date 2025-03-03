9578b86bfb85f36a3ba165084d29a85ce6320a40
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-03 11:37:15 -0500

    feat: added padding to homepage for smaller screens

--- src/app/components/page_sections/HomeSection.tsx
+++ src/app/components/page_sections/HomeSection.tsx
@@ -44,47 +44,49 @@ export default function HomeSection() {
           <path d="M500 50.7C358 38 0 4 0 4V0h1000v40.7C784 64 642 63.3 500 50.7Z"></path>
         </g>
       </svg> */}
-      <div className="flex items-center gap-2">
-        <FontAwesomeIcon icon={faLocationArrow} className="w-4 text-red-800" />
-        <h3>Toronto, ON</h3>
-      </div>
-      <h1 className="flex flex-col leading-snug">
-        <span className="text-xl sm:text-2xl">Hello,</span> I&apos;m Mohamed
-      </h1>
-      <h2 className="text-base sm:text-xl">
-        Software Engineer | Web Developer | Problem Solver
-      </h2>
-      <div className="flex justify-center gap-4 mt-2">
-        <Link
-          className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
-          href="https://github.com/moibra05"
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
-          />
-        </Link>
-        <Link
-          className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
-          href="https://www.linkedin.com/in/moibra2005/"
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
-          />
-        </Link>
-        <Link
-          className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
-          href="mailto:mohamedhassen.ibrahim@mail.utoronto.ca"
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
-          />
-        </Link>
+      <div className="p-4">
+        <div className="flex items-center gap-2">
+          <FontAwesomeIcon icon={faLocationArrow} className="w-4 text-red-800" />
+          <h3>Toronto, ON</h3>
+        </div>
+        <h1 className="flex flex-col leading-snug">
+          <span className="text-xl sm:text-2xl">Hello,</span> I&apos;m Mohamed
+        </h1>
+        <h2 className="text-base sm:text-xl">
+          Software Engineer | Web Developer | Problem Solver
+        </h2>
+        <div className="flex justify-center gap-4 mt-2">
+          <Link
+            className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
+            href="https://github.com/moibra05"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
+            />
+          </Link>
+          <Link
+            className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
+            href="https://www.linkedin.com/in/moibra2005/"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
+            />
+          </Link>
+          <Link
+            className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
+            href="mailto:mohamedhassen.ibrahim@mail.utoronto.ca"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="w-4 h-4 sm:w-6 sm:h-6 fa-fw text-white group-hover:text-secondary transition-colors duration-300"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
