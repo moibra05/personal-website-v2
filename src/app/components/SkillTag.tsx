62b68a5c026a3ab0d922abfb8b8ddef722df8704
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-02 20:28:28 -0500

    feat: created fallback image for skills if image not found

--- src/app/components/SkillTag.tsx
+++ src/app/components/SkillTag.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 type Prop = {
@@ -5,16 +8,19 @@ type Prop = {
 };
 
 export default function SkillTag({ skill }: Prop) {
+  const [imgSrc, setImgSrc] = useState(
+    `/tools-icons/${skill.toLowerCase().replace(/[^a-z0-9]/g, "-")}.png`
+  );
+
   return (
     <span className="flex items-center px-3 py-1 bg-secondary font-bold text-white rounded-xl text-base md:text-lg hover:cursor-default hover:scale-105 transition-transform duration-300">
       <Image
-        src={`/tools-icons/${skill
-          .toLowerCase()
-          .replace(/[^a-z0-9]/g, "-")}.png`}
+        src={imgSrc}
         alt={skill}
         width={24}
         height={24}
         className="inline-block mr-2 hue-rotate-180 filter invert brightness-200"
+        onError={() => setImgSrc("/tools-icons/default-icon.png")}
       />
 
       {skill}
