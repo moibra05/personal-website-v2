79a349090a4d224832551c79ec0778e1cf8863ad
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-02 23:02:57 -0500

    feat: created contact form frontend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ export default {
       },
       boxShadow: {
         "navbar-shadow": "0 0px 50px rgba(60, 131, 246, 0.4)",
+        "contact-form-shadow": "0 0 500px rgba(255, 255, 255, 0.05)",
       }
     },
   },
