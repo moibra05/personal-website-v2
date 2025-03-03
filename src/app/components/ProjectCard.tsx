c2c0c8f2c27a7ebc9cee1478df38bcf9b1f23aa7
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-02 20:28:56 -0500

    feat: adjusted skills tags dispaly

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -18,23 +18,25 @@ export default function ProjectCard({ title, description, url, tags }: Props) {
         className="flex-shrink-0"
       >
         <Image
-          src={`/placeholder.png`}
-          width={400}
+          src={`/project-images/${title
+            .toLowerCase()
+            .replace(/\s+/g, "-")}.png`}
+          width={475}
           height={200}
           alt={title}
-          className="rounded-md"
+          className="rounded-md border-gray-700 border-2 hover:scale-[1.01] transition-transform duration-300 ease-in-out"
         />
       </Link>
       <div className="flex flex-col gap-4 md:-ml-[12%] md:group-even:ml-0 md:group-even:-mr-[12%]">
         <h3 className="text-xl md:text-2xl font-bold md:text-right group-even:text-left">
           {title}
         </h3>
-        <div className="bg-textGray opacity-95 rounded-md p-4 md:p-8 md:pl-16 md:group-even:pl-8 md:group-even:pr-16 shadow-2xl">
+        <div className="bg-textGray opacity-95 rounded-md p-4 md:p-6 md:pl-16 md:group-even:pl-8 md:group-even:pr-16 shadow-2xl">
           <p className="text-primary font-bold text-base text-justify">
             {description}
           </p>
         </div>
-        <div className="flex gap-2 md:gap-4">
+        <div className="flex gap-2 md:gap-4 z-50 flex-wrap md:group-odd:flex-row-reverse">
           {tags.map((tag) => (
             <SkillTag key={tag} skill={tag} />
           ))}
