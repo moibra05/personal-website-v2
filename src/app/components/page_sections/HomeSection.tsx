8e4ae7beadff639ff081b919c0665711ce2e3f1e
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2025-03-03 13:30:10 -0500

    feat: created background design for homepage

--- src/app/components/page_sections/HomeSection.tsx
+++ src/app/components/page_sections/HomeSection.tsx
@@ -7,47 +7,12 @@ export default function HomeSection() {
   return (
     <div
       id="home"
-      className="flex flex-col w-fit h-screen justify-center mx-auto"
+      className="flex flex-col w-full h-screen justify-center"
     >
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1000 100"
-        className="absolute w-screen left-0 top-0 z-40"
-      >
-        <g fill="#2C68FF">
-          <path
-            d="M500 80.7C358 68 0 4 0 4V0h1000v84.7c-216 23.3-358 8.6-500-4Z"
-            opacity=".3"
-          ></path>
-          <path
-            d="M500 65.7C358 53 0 4 0 4V0h1000v62.7c-216 23.3-358 15.6-500 3Z"
-            opacity=".5"
-          ></path>
-          <path d="M500 50.7C358 38 0 4 0 4V0h1000v40.7C784 64 642 63.3 500 50.7Z"></path>
-        </g>
-      </svg>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1000 100"
-        className="absolute w-screen left-0 -bottom-0 z-40 max-h-["
-      >
-        <g fill="#2C68FF" transform="rotate(180 500 50)">
-          <path
-            d="M500 80.7C358 68 0 4 0 4V0h1000v84.7c-216 23.3-358 8.6-500-4Z"
-            opacity=".3"
-          ></path>
-          <path
-            d="M500 65.7C358 53 0 4 0 4V0h1000v62.7c-216 23.3-358 15.6-500 3Z"
-            opacity=".5"
-          ></path>
-          <path d="M500 50.7C358 38 0 4 0 4V0h1000v40.7C784 64 642 63.3 500 50.7Z"></path>
-        </g>
-      </svg> */}
-      <div className="p-4">
-        <div className="flex items-center gap-2">
+      <div className="p-4 w-fit mx-auto">
+        <div className="flex items-center gap-2 mb-2">
           <FontAwesomeIcon icon={faLocationArrow} className="w-4 text-red-800" />
-          <h3>Toronto, ON</h3>
+          <h3 className="text-base sm:text-lg">Toronto, ON</h3>
         </div>
         <h1 className="flex flex-col leading-snug">
           <span className="text-xl sm:text-2xl">Hello,</span> I&apos;m Mohamed
@@ -55,7 +20,7 @@ export default function HomeSection() {
         <h2 className="text-base sm:text-xl">
           Software Engineer | Web Developer | Problem Solver
         </h2>
-        <div className="flex justify-center gap-4 mt-2">
+        <div className="flex justify-center gap-4 mt-4">
           <Link
             className="text-white border-[3px] rounded-full p-1 group hover:cursor-pointer hover:border-secondary transition-colors duration-300"
             href="https://github.com/moibra05"
